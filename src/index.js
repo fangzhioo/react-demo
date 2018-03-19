53f440c184091a8295906d995f894f136b9a920c
fangzhi <1020637302@qq.com>
2018-03-19 17:37:58 +0800

    模拟实现 redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import CommentApp from "./comment/CommentApp"
 import './index.css';
 
+// make redux
+require("./makeRedux/index");
+
 ReactDOM.render(
     <CommentApp />,
     document.getElementById('root')
