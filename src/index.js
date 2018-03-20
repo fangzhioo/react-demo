98e62b802167778654b0ed1505d55158e54731b8
fangzhi <1020637302@qq.com>
2018-03-20 11:48:38 +0800

    结合 context 和 store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentApp from "./comment/CommentApp"
+import Index from "./makeRedux/Index"
 import './index.css';
 
 // make redux
-require("./makeRedux/index");
+require("./makeRedux/makeredux");
 
+// context and redux
+ReactDOM.render(
+  <Index />,
+  document.getElementById('context')
+);
+
+// comment app
 ReactDOM.render(
     <CommentApp />,
     document.getElementById('root')
   );
+  
   
--- src/makeRedux/index.js
+++ src/makeRedux/index.js
@@ -1,79 +1,53 @@
-function createStore (state, stateChanger) {
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Header from "./Header"
+import Content from "./Content"
+
+function createStore (reducer) {
+    let state = null
     const listeners = []
     const subscribe = (listener) => listeners.push(listener)
     const getState = () => state
     const dispatch = (action) => {
-      state = stateChanger(state, action) // 覆盖原对象
+      state = reducer(state, action)
       listeners.forEach((listener) => listener())
     }
+    dispatch({}) // 初始化 state
     return { getState, dispatch, subscribe }
   }
   
-  function renderApp (newAppState, oldAppState = {}) { // 防止 oldAppState 没有传入，所以加了默认参数 oldAppState = {}
-    if (newAppState === oldAppState) return // 数据没有变化就不渲染了
-    console.log('render app...')
-    renderTitle(newAppState.title, oldAppState.title)
-    renderContent(newAppState.content, oldAppState.content)
-  }
-  
-  function renderTitle (newTitle, oldTitle = {}) {
-    if (newTitle === oldTitle) return // 数据没有变化就不渲染了
-    console.log('render title...')
-    const titleDOM = document.getElementById('title')
-    titleDOM.innerHTML = newTitle.text
-    titleDOM.style.color = newTitle.color
-  }
-  
-  function renderContent (newContent, oldContent = {}) {
-    if (newContent === oldContent) return // 数据没有变化就不渲染了
-    console.log('render content...')
-    const contentDOM = document.getElementById('content')
-    contentDOM.innerHTML = newContent.text
-    contentDOM.style.color = newContent.color
-  }
-  
-  let appState = {
-    title: {
-      text: '这是一个标题',
-      color: 'red',
-    },
-    content: {
-      text: '这是一个内容',
-      color: 'blue'
+  const themeReducer = (state, action) => {
+    if (!state) return {
+      themeColor: 'red'
     }
-  }
-  
-  function stateChanger (state, action) {
     switch (action.type) {
-      case 'UPDATE_TITLE_TEXT':
-        return { // 构建新的对象并且返回
-          ...state,
-          title: {
-            ...state.title,
-            text: action.text
-          }
-        }
-      case 'UPDATE_TITLE_COLOR':
-        return { // 构建新的对象并且返回
-          ...state,
-          title: {
-            ...state.title,
-            color: action.color
-          }
-        }
+      case 'CHANGE_COLOR':
+        return { ...state, themeColor: action.themeColor }
       default:
-        return state // 没有修改，返回原来的对象
+        return state
     }
   }
   
-  const store = createStore(appState, stateChanger)
-  let oldState = store.getState() // 缓存旧的 state
-  store.subscribe(() => {
-    const newState = store.getState() // 数据可能变化，获取新的 state
-    renderApp(newState, oldState) // 把新旧的 state 传进去渲染
-    oldState = newState // 渲染完以后，新的 newState 变成了旧的 oldState，等待下一次数据变化重新渲染
-  })
-  
-  renderApp(store.getState()) // 首次渲染页面
-  store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '修改后的标题' }) // 修改标题文本
-  store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
+const store = createStore(themeReducer)
+
+class Index extends Component {
+    static childContextTypes = {
+        store: PropTypes.object
+    } // 定义一个指定类型的store
+
+    getChildContext(){
+        return { store }
+    } // 获取context
+
+    render(){
+        return (
+            <div>
+                <Header /> 
+                <Content /> 
+            </div>
+        )
+    }
+
+}
+
+export default Index;
